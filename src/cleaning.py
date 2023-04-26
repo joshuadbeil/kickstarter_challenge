8a0c8ba3b0eb08737fa51e5f11d678edd2a200f8
Joshua Beilenhoff <j.beilenhoff@gmail.com>
2023-04-27 01:40:45 +0200

    getting rid of the cleaning.py bug

--- src/cleaning.py
+++ src/cleaning.py
@@ -148,26 +148,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-=======
-import pandas as pd
-
-
-def drop_duplicates(
-    dataframe,
-    duplicate_column="id",
-    feature_column="usd_type",
-    feature_value="domestic",
-):
-    # Identify the duplicated rows based on the "id" column
-    duplicate_mask = dataframe.duplicated("id")
-
-    # Select the rows that are not duplicates and do not have "domestic" as the value in the "usd_type" column
-    dataframe = dataframe[
-        ~((duplicate_mask) & (dataframe[feature_column] == feature_value))
-    ]
-
-    # Drop the rows that are duplicates based on the "id" column, keeping only the first occurrence of each group
-    dataframe.drop_duplicates(duplicate_column, keep="first", inplace=True)
-
-    # Return the resulting DataFrame
-    return dataframe
